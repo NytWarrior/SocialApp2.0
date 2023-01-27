7e54111c73e5784f7adeec41b5fa13eec5846e9a
Rajeev <rajeevkr8917@gmail.com>
2023-01-28 01:11:12 +0530

    schema of post and saving it to db

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const router = express.Router()
 const homeController = require('../controllers/home_Controller');
 
 
-
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+
+//for any further routes, access from here
+//router.use('/routerName', reuire('./routerfile))
 
 module.exports = router;
