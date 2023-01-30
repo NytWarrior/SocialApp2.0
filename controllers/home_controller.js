41add9b12228c8aafe92a4753ae905a1852c0c18
Rajeev <rajeevkr8917@gmail.com>
2023-01-31 01:04:46 +0530

    set the code using async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,34 +1,30 @@
 const Post = require('../models/post')
 const User = require('../models/user');
 
-module.exports.home = function (req, res) {
-    // return res.end('<h1>Express is up!!</h1>')
+module.exports.home = async function (req, res) {
 
-    // Post.find({}, function (err, posts) {
-    //     return res.render('home', {
-    //         title: "Home",
-    //         posts: posts
-    //     })
-    // })
-
-    //populate the user of each post(it will show all the detail of the user)
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        })
-        .exec(function (err, posts) {
-            User.find({}, function (err, users) {
-                return res.render('home', {
-                    title: "Home",
-                    posts: posts,
-                    all_users: users
-                });
+    try {
+        //populate the user of each post(it will show all the detail of the user)
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
             });
-        })
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Home",
+            posts: posts,
+            all_users: users
+        });
+    }
+    catch (err) {
+        console.log('Error', err);
+    }
 }
 
 
