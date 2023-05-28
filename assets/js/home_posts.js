44dffd43828a046eca56b3485540d9d2cd98063f
Rajeev <rajeevkr8917@gmail.com>
2023-05-28 10:36:54 +0530

    screenshot added

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -19,7 +19,7 @@
                 data: form,
                 success: function (data) {
                     let newPost = newPostDom(data.data.post);
-                    $('#posts-list-container>div').prepend(newPost);
+                    $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost));
 
                     // call the create comment class
@@ -50,13 +50,14 @@
         let postImage, postVideo;
         try {
             if (post.image !== "") {
-                postImage = `<img class="img-fluid" src="${post.image}" alt="alt-post-image" />`;
+                postImage = `<img class="img-fluid" src="${post.image}" alt="alt-post-image" loading="lazy" />`;
+
             } else {
                 postImage = "";
             }
 
             if (post.video !== "") {
-                postVideo = `<video class="img-fluid" src="${post.video}" alt="alt-post-video" controls />`;
+                postVideo = `<video class="img-fluid" src="${post.video}" alt="alt-post-video" controls>`;
             } else {
                 postVideo = "";
             }
@@ -64,89 +65,68 @@
             console.log("error", error);
         }
 
-        return $(`<link rel="stylesheet" href="/css/post.css">
-        <div class=" gedf-main" data-bs-theme="dark" id="post-${post._id}">
-                <div class="card gedf-card">
-                    <div class="card-header" style="background-color: #181818;">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="mr-2">
-                                    <img class="rounded-circle" width="45" src="${post.user.avatar}" alt="">
-                                </div>
-                                <div class="ml-2">
-                                    <div class="h5 m-1">
-                                        ${post.user.name}
-                                    </div>
-                                    <div class="h7 text-muted">Location</div>
-                                    <!-- <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div> -->
-                                </div>
-                            </div>
-                            <div>
-                            <div class="dropdown">
-                                            <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1"
-                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="fa fa-ellipsis-h"></i>
-                                            </button>
-                                            <div class="dropdown-menu" aria-labelledby="gedf-drop1">
-                                                <!-- <div class="h6 dropdown-header">Configuration</div> -->
-                                                <a class="delete-post-button dropdown-item"
-                                                    href="/posts/destroy/${post._id}">Delete</a>
-                                            </div>
-
+        return $(`<li class=" gedf-main" id="post-${post._id}" data-bs-theme="dark">
+                        <div class="card gedf-card">
+                            <div class="card-header" style="background-color: #181818;">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="mr-2">
+                                            <img class="rounded-circle" width="45" src="${post.user.avatar}" alt="">
                                         </div>
-                                
+                                        <div class="ml-2">
+                                            <div class="h5 m-1">
+                                                ${post.user.name}
+                                            </div>
+                                            <div class="h7 text-muted">Location</div>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="dropdown">
+                                    <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-ellipsis-h"></i>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="gedf-drop1">
+                                        <a class="delete-post-button dropdown-item" href="/posts/destroy/${post._id}">Delete</a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                            <div class="card-body">
+                                <p class="card-title">
+                                    ${post.content}
+                                </p>
 
-                    </div>
-                    <div class="card-body">
+                                <p class=" card-image">
+                                    ${postImage}
+                                    ${postVideo}                   
+                                </p>
+                            </div >
 
-                        <p class="card-title">
-                            ${post.content}
-                        </p>
+                            <div class="card-footer" style="font-size: small; background-color: #181818;">
+                        
+                                <a href="/likes/toggle/?id=${post._id}&type=Post " class="card-link toggle-like-button"
+                                    data-likes="0"><i class="fa fa-gittip"></i> 0 Like </a>
 
-                        <p class=" card-image">
-
-                                `<% if (post.image) { %>
-                                    <img class="img-fluid" src="${postImage}" alt="alt-post-image" loading="lazy" />
-                                    <% } %>
-                                        <% if (post.video) { %>
-            <video class="img-fluid" src="${postVideo}" alt="alt-post-video" controls>
-                <% } %>`
-            </p>
-                    </div >
-
-                    <div class="card-footer" style="font-size: small; background-color: #181818;">
-                      
-                <a href="/likes/toggle/?id=${post._id}&type=Post " class="card-link toggle-like-button"
-                    data-likes="0"><i class="fa fa-gittip"></i>
-                    0 Like
-                </a>
-
-                <a class="card-link" data-bs-toggle="collapse" href="#collapseExample-${post._id}">
-                    <i class="fa fa-comment-alt"></i>Comment
-                </a>
-                
-                        <!-- <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a> -->
-                    </div>
-                    <div class="collapse" id="collapseExample-${post._id}">
-                        <div class="card">
+                                <a class="card-link" data-bs-toggle="collapse" href="#collapseExample-${post._id}">
+                                    <i class="fa fa-comment-alt"></i>Comment</a>
                     
-                            <section class="mt-3">
-                                <form action="/comments/create" method="POST">
-                                    <div class="input-group input-group">
-                                        <input type="text" class="form-control" name="content" placeholder="Write Comment"
-                                            style="background-color: #181818" aria-label="Recipient's username"
-                                            aria-describedby="basic-addon2" required>
-                                            <input type="hidden" name="post" value="${post._id}">
+                            </div>
+                            <div class="collapse" id="collapseExample-${post._id}">
+                                <div class="card">
+                        
+                                    <section class="mt-3">
+                                        <form action="/comments/create" method="POST">
+                                            <div class="input-group input-group">
+                                                <input type="text" class="form-control" name="content" placeholder="Write Comment"
+                                                    style="background-color: #181818" aria-label="Recipient's username"
+                                                    aria-describedby="basic-addon2" required>
+                                                <input type="hidden" name="post" value="${post._id}">
                                                 <div class="input-group-append">
-                                                    <input class="text-decoration-none text-black btn btn-warning" type="submit"
-                                                        value="Add Comment">
+                                                    <input class="text-decoration-none text-black btn btn-warning" type="submit" value="Add Comment">
                                                 </div>
                                             </div>
                                         </form>
                                     </section>
-                                    <!-- comment card bgins -->
+                                     
                                     <section style="background-color: #181818">
                                         <div class="post-comments">
                                             <div class="post-comments-list">
@@ -156,9 +136,9 @@
                                         </div>
                                     </section>
                                 </div>
+                            </div>
                         </div>
-                    </div>
-                </div > `)
+                    </li > `)
     }
 
 
@@ -171,7 +151,7 @@
                 type: 'get',
                 url: $(deleteLink).prop('href'),
                 success: function (data) {
-                    $(`#post - ${ data.data.post_id } `).remove();
+                    $(`#post - ${data.data.post_id} `).remove();
                     new Noty({
                         theme: 'relax',
                         text: "Post Deleted",
@@ -194,7 +174,7 @@
 
     // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
     let convertPostsToAjax = function () {
-        $('#posts-list-container>div').each(function () {
+        $('#posts-list-container>ul>li').each(function () {
             let self = $(this);
             let deleteButton = $(' .delete-post-button', self);
             deletePost(deleteButton);
